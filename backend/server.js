50201f389d36c65b842c76577cc98674f062bf09
adamEllisMacBook <125833852+adamEllisMacBook@users.noreply.github.com>
2024-08-14 19:43:22 +0100

    up to json web token

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config() // call above where we uer the variables
 import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 const app = express()
 const port = process.env.PORT || 5001 // fallback
+
+// body parser middlewart
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 connectDB() // call at begining of app
 
@@ -14,6 +21,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // make sure under all routes
 app.use(notFound)
