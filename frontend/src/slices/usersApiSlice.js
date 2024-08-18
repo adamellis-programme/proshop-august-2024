0ebec580b03279a991ccf4e2f8f5dad6a1e594ec
adamEllisMacBook <125833852+adamEllisMacBook@users.noreply.github.com>
2024-08-18 13:27:48 +0100

    up to display orders

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -26,7 +26,21 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: 'POST',
       }),
     }),
+
+    // mutation to update the profile
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
   }),
 })
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation, // used in profiles screen
+} = usersApiSlice
