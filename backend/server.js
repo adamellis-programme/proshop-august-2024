37af0ed575a9ab2435743b946b3387d30e84c6f1
adamEllisMacBook <125833852+adamEllisMacBook@users.noreply.github.com>
2024-10-09 18:18:50 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ app.use(cookieParser())
 
 connectDB() // call at begining of app
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -69,6 +65,19 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 // For example, if your project directory is /home/user/project, this line will resolve the uploads directory as /home/user/project/uploads.
 
 // make sure under all routes
+
+// ANY OTHER ROUTES THAT DO NOT MATCH GET SENT HERE
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 app.use(notFound)
 app.use(errorHandler)
 app.listen(port, () => {
